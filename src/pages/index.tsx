7e5cfa1532df835882af57c54bef07491288dc67
OxMarco <>
2022-03-30 01:21:52 +0400

    motto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const IndexPage = () => (
     {/* <SEO title="Home" /> */}
     <div tw="h-[calc(100% - 100px)] w-full flex flex-col tablet:flex-row justify-center items-center gap-10">
       <div tw="flex flex-col w-4/5 tablet:w-2/5 gap-5">
-        <Txt.Section>DeFi made <i>easy</i></Txt.Section>
+        <Txt.Section>Leverage to <i>earn</i></Txt.Section>
         <Txt.Body2Regular tw="text-font-200">To experienced traders and DeFi newcomers entering the market or looking for customizable investment strategies, our DeFi protocol offers simplified and reliable leveraged staking and trading strategies for any token, because we believe in simplicity and guidance in the complex Defi universe.</Txt.Body2Regular>
         <div tw="flex flex-row m-4 gap-3 ml-0">
           <Button text='Start investing' action bold />
