3186a7ae5435394aa2b2816920353eaa8fcfbc2b
OxMarco <>
2022-04-15 16:00:51 +0200

    netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     },
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-netlify`,
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
