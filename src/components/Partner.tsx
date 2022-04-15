e770de8d2c1eb7de09d85aa8a25c07e59e06e972
Valentin Kuharic <valentinkuharic12@gmail.com>
2022-04-15 10:57:20 +0200

    Partners images added and minor CSS fixes

--- src/components/Partner.tsx
+++ src/components/Partner.tsx
@@ -10,7 +10,7 @@ const Partner = (props: { image: any }) => {
   const { image } = props
   const Image = image
   return (
-    <div tw="rounded-xl bg-[rgba(255, 255, 255, 0.1)] flex flex-col justify-between py-3 tablet:px-4 tablet:py-7 min-width[110px] tablet:min-width[180px] desktop:min-width[240px] justify-center items-center">
+    <div tw="rounded-xl bg-[rgba(255, 255, 255, 0.1)] flex flex-col justify-between py-3 tablet:px-4 tablet:py-7 width[110px] tablet:width[180px] desktop:width[240px] height[50px] tablet:height[90px] desktop:height[110px] justify-center items-center">
       <img tw='w-8/12 h-auto' src={Image} alt="token icon" />
     </div>
   )
