028f0ac392c5bdaa04f504d02180ceb441c096d7
Valentin Kuharic <valentinkuharic12@gmail.com>
2022-04-28 09:59:53 +0200

    Removed hotjar and google analytcs, added google tag manager and base config

--- src/components/TrackedLink.tsx
+++ src/components/TrackedLink.tsx
@@ -10,12 +10,6 @@ export const TrackedLink = (props: {
 }) => {
   const { title, target, url } = props
 
-  /*
-    const sendRequest = useCallback(async () => {
-        hotjar.event(`${title} button was clicked to visit url ${url}`);
-    }, [url])
-    */
-
   return (
     <a href={url} target={target} title={title} {...props}>
       {props.children}
